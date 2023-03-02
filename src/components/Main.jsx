17a910f50eb9beb6a5e1e7dcaf0fe009048f4f9f
Charlito Mecarez Jr <70713746+javaplumdev@users.noreply.github.com>
2023-03-02 11:55:02 +0800

    update

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,13 +4,22 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { ContextProvider } from '../context/context';
 import Modal from 'react-bootstrap/Modal';
+import { BsPencilFill, BsTrashFill } from 'react-icons/bs';
 
 const Main = () => {
-	const { lists, addItem, show, setShow, handleClose, handleShow } =
-		useContext(ContextProvider);
+	const {
+		lists,
+		addItem,
+		show,
+		removeItem,
+		handleClose,
+		handleShow,
+		setChecked,
+		checked,
+	} = useContext(ContextProvider);
 
 	const [title, setTitle] = useState('');
-	const [status, setStatus] = useState('');
+	const [status, setStatus] = useState('Incomplete');
 
 	const changeStatus = (status) => {
 		setStatus(status);
@@ -40,12 +49,45 @@ const Main = () => {
 							</Dropdown.Menu>
 						</Dropdown>
 					</div>
-					<div className="bg-secondary mt-3 rounded p-3 text-white">
+					<div
+						className="mt-3 rounded p-3 text-white "
+						style={{ backgroundColor: '#ECEDF6' }}
+					>
 						{lists.length === 0 ? (
-							<p>No todos</p>
+							<p className="text-center text-dark">No todos</p>
 						) : (
 							lists.map((item) => {
-								return <div key={item.name}>{item.title}</div>;
+								return (
+									<div
+										key={item.id}
+										className="text-dark bg-white p-2 rounded d-flex justify-content-between my-2 align-items-center"
+									>
+										<div className="d-flex">
+											<input
+												type="checkbox"
+												style={{ width: '30px' }}
+												defaultChecked={checked}
+												onChange={() => setChecked(!checked)}
+											/>
+											<div className="mx-2">
+												{item.title}
+												<br></br>
+												{item.dateToday}
+											</div>
+										</div>
+										<div>
+											<button
+												className="mx-3 p-2 border-0 text-dark rounded"
+												onClick={() => removeItem(item.id)}
+											>
+												<BsTrashFill />
+											</button>
+											<button className="p-2 border-0 text-dark rounded">
+												<BsPencilFill />
+											</button>
+										</div>
+									</div>
+								);
 							})
 						)}
 					</div>
@@ -66,7 +108,7 @@ const Main = () => {
 					<Form.Label className="mt-3">Status</Form.Label>
 					<Dropdown>
 						<Dropdown.Toggle id="dropdown-basic" className="text-white w-100">
-							All
+							{status}
 						</Dropdown.Toggle>
 
 						<Dropdown.Menu>
--- src/context/context.js
+++ src/context/context.js
@@ -12,21 +12,69 @@ export const ContextFunction = ({ children }) => {
 	const handleClose = () => setShow(false);
 	const handleShow = () => setShow(true);
 
+	const [checked, setChecked] = useState(false);
+
+	const months = [
+		'1',
+		'2',
+		'3',
+		'4',
+		'5',
+		'6',
+		'7',
+		'8',
+		'9',
+		'10',
+		'11',
+		'12',
+	];
+
 	const addItem = (title, status) => {
+		const id = Math.floor(Math.random() * 10000);
+
+		const date = new Date();
+
+		let month = months[date.getMonth()];
+
 		setLists((prevState) => [
 			...prevState,
 			{
+				id: id,
 				title: title,
 				status: status,
+				dateToday: month + '/' + date.getDate() + '/' + date.getFullYear(),
 			},
 		]);
 
-		console.log(lists);
+		if (status === 'Completed') {
+			setChecked(true);
+		} else if (status === 'Incomplete') {
+			setChecked(false);
+		}
+
+		handleClose();
+	};
+
+	const removeItem = (id) => {
+		const filteredItem = lists.filter((item) => item.id !== id);
+
+		setLists(filteredItem);
 	};
 
 	return (
 		<ContextProvider.Provider
-			value={{ sayHi, addItem, show, setShow, handleClose, handleShow, lists }}
+			value={{
+				sayHi,
+				addItem,
+				show,
+				setShow,
+				handleClose,
+				handleShow,
+				lists,
+				removeItem,
+				setChecked,
+				checked,
+			}}
 		>
 			{children}
 		</ContextProvider.Provider>
